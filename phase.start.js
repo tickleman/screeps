8365709dfcb95a7a7a9a3bac7c21ffd38288c328
Baptiste Pillot <baptiste.pillot@bappli.com>
2016-07-09 01:08:10 +0200

    Start phase : create creep only if there is not already one

--- phase.start.js
+++ phase.start.js
@@ -2,8 +2,8 @@ var sources = require('sources');
 
 module.exports.run = function()
 {
-	if (Game.spawns.Spawn.canCreateCreep([CARRY, MOVE, WORK])) {
-		sources.memorize();
+	if (!_.filter(Game.creeps).length) {
+		sources.memorize(true);
 		var closest_source = Game.spawns.Spawn.pos.findClosestByPath(FIND_SOURCES_ACTIVE).id;
 		var creep_name = Game.spawns.Spawn.createCreep([CARRY, MOVE, WORK], undefined, {
 			role: 'harvester', source: closest_source, target: Game.spawns.Spawn.id
--- sources.js
+++ sources.js
@@ -6,14 +6,18 @@ module.exports =
 	/**
 	 * List all available sources
 	 * List all accessible points to these sources (capacity)
+	 *
+	 * @param force boolean if set to true, refresh existing sources memory
 	 */
-	memorize: function()
+	memorize: function(force)
 	{
-		Memory.sources = {};
-		for (var room_key in Game.rooms) {
-			Game.rooms[room_key].find(FIND_SOURCES_ACTIVE).forEach(function(source) {
-				Memory.sources[source.id] = new Source(source);
-			});
+		if (force || !Memory.sources) {
+			Memory.sources = {};
+			for (var room_key in Game.rooms) {
+				Game.rooms[room_key].find(FIND_SOURCES_ACTIVE).forEach(function (source) {
+					Memory.sources[source.id] = new Source(source);
+				});
+			}
 		}
 	}
 
