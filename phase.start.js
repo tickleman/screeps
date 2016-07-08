1a4c3f1607e92f4a1cb1c63936367a2eeb9b9f41
Baptiste Pillot <baptiste.pillot@bappli.com>
2016-07-09 00:41:04 +0200

    Fix new creep creation process

--- phase.start.js
+++ phase.start.js
@@ -4,9 +4,9 @@ module.exports.run = function()
 {
 	sources.memorize();
 	var closest_source = Game.spawns.Spawn.pos.findClosestByPath(FIND_SOURCES_ACTIVE).id;
-	var creep = Game.spawns.Spawn.createCreep([WORK, CARRY, MOVE], undefined, {
+	var creep_name = Game.spawns.Spawn.createCreep([WORK, CARRY, MOVE], undefined, {
 		role: 'harvester', source: closest_source
 	});
-	Memory.sources[closest_source][0].creep = creep.id;
+	Memory.sources[closest_source].terrains[0].creep = Game.creeps[creep_name].id;
 	Memory.phase = 'harvest';
 };
