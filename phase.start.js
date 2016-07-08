eca8c6db830286d221fe32171862d0e13daaac1e
Baptiste Pillot <baptiste.pillot@bappli.com>
2016-07-09 01:05:03 +0200

    Start phase : create creep only when we can

--- phase.start.js
+++ phase.start.js
@@ -2,11 +2,15 @@ var sources = require('sources');
 
 module.exports.run = function()
 {
-	sources.memorize();
-	var closest_source = Game.spawns.Spawn.pos.findClosestByPath(FIND_SOURCES_ACTIVE).id;
-	var creep_name = Game.spawns.Spawn.createCreep([CARRY, MOVE, WORK], undefined, {
-		role: 'harvester', source: closest_source, target: Game.spawns.Spawn.id
-	});
-	Memory.sources[closest_source].terrains[0].creep = Game.creeps[creep_name].id;
-	Memory.phase = 'harvest';
+	if (Game.spawns.Spawn.canCreateCreep([CARRY, MOVE, WORK])) {
+		sources.memorize();
+		var closest_source = Game.spawns.Spawn.pos.findClosestByPath(FIND_SOURCES_ACTIVE).id;
+		var creep_name = Game.spawns.Spawn.createCreep([CARRY, MOVE, WORK], undefined, {
+			role: 'harvester', source: closest_source, target: Game.spawns.Spawn.id
+		});
+		Memory.sources[closest_source].terrains[0].creep = Game.creeps[creep_name].id;
+	}
+	if (_.filter(Game.creeps).length) {
+		Memory.phase = 'harvest';
+	}
 };
