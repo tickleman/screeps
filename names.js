d6b38121f787cca76d727773635c44bbfce1a036
Baptiste Pillot <baptiste@pillot.fr>
2016-07-09 21:21:25 +0200

    Log names

--- names.js
+++ names.js
@@ -13,10 +13,14 @@ module.exports.chooseName = function()
 {
 	var name;
 	for (var i = 0; i < this.use_names.length; i ++) {
+		console.log('are there available names for ' + this.use_names[i] + ' ?');
 		if (Game.creeps.length < this[this.use_names[i]].length) {
+			console.log('yes');
 			for (; !name || Game.creeps[name]; name = this.names[Math.floor(Math.random() * this.names.length)]) {}
+			console.log('choose name ' + name);
 			return name;
 		}
 	}
+	console.log('no available name');
 	return undefined;
 };
