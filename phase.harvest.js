7546dd07c700179fdd15f08ff42a8da5a8d07066
Baptiste Pillot <baptiste@pillot.fr>
2016-07-09 15:17:50 +0200

    Make builders work and replace upgraders

--- phase.harvest.js
+++ phase.harvest.js
@@ -13,10 +13,12 @@ var sources   = require('sources');
  */
 module.exports.run = function()
 {
+
 	var creeps = _.filter(Game.creeps, creep => (
 		(creep.memory.role == 'builder') || (creep.memory.role == 'harvester') || (creep.memory.role == 'upgrader')
 	));
 	var count = { builder: 0, harvester: 0, upgrader: 0 };
+
 	// make creeps work, and count them
 	for (var name in creeps) {
 		var creep = creeps[name];
@@ -25,17 +27,21 @@ module.exports.run = function()
 		if (creep.memory.role == 'upgrader')  upgrader.work(creep);
 		count[creep.memory.role] ++;
 	}
+
 	// priority : 2 harvesters
 	if (count['harvester'] < Math.min(sources.terrainsCount(), 2)) {
 		harvester.spawn();
 	}
+
 	else {
+
 		// if there are construction sites : builders : 1 per construction site
 		var construction_sites = Game.spawns.Spawn.room.find(FIND_CONSTRUCTION_SITES);
 		if (count['builder'] < construction_sites.length) {
 			builder.spawn(Game.spawns.Spawn.room.find(FIND_CONSTRUCTION_SITES)[count['builder']].id);
 		}
-		// and last, upgraders : only if they are not replaced by builders
+
+		// and last, upgraders : at least 1, more if they are not replaced by builders
 		else {
 			// if there is no construction sites : 1.5x more upgraders than free construction sites
 			var more_upgraders = !construction_sites.length ? 1.5 : 1;
@@ -44,5 +50,6 @@ module.exports.run = function()
 				upgrader.spawn();
 			}
 		}
+
 	}
 };
