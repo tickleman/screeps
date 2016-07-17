57509575cc7d64b344d5b83ebb532b3f93a8a559
Baptiste Pillot <baptiste@pillot.fr>
2016-07-17 19:53:19 +0200

    Task-creep work : fix #3 and a lot of debugging information

--- creep.js
+++ creep.js
@@ -328,26 +328,32 @@ module.exports.targets = function(creep)
  **/
 module.exports.work = function(creep)
 {
-	if (creep.memory.task != undefined) {
+	if (creep.memory.task !== undefined) {
+		if (this.DEBUG) console.log(creep.name + ' task ' + creep.memory.task);
 		var task = Memory.tasks[creep.memory.task];
 		// prepare to go to init start point or start init sequence in already in start point
 		if (!creep.memory.step) {
 			creep.memory.step = 'go';
 			creep.memory.path = path.calculate(creep, path.startRoomPosition(task.init));
+			if (this.DEBUG) console.log('- go ' + creep.memory.path);
 			creep.memory.step_pos = 4;
 			if (creep.memory.step_pos >= creep.memory.path) {
 				delete creep.memory.path;
 				creep.memory.step = 'init';
+				if (this.DEBUG) console.log('- init instead of go ' + task.init);
 			}
 		}
 		// go to init start point
 		if (creep.step == 'go') {
+			if (this.DEBUG) console.log('- go ' + creep.memory.path + ' step ' + creep.memory.step_pos);
 			if (!creep.move(creep.memory.path[creep.memory.step_pos])) {
+				if (this.DEBUG) console.log('- next step');
 				creep.memory.step_pos ++;
 				if (creep.memory.step_pos >= creep.memory.path.length) {
 					delete creep.memory.path;
 					creep.memory.step = 'init';
 					creep.memory.step_pos = 4;
+					if (this.DEBUG) console.log('- init ' + task.init);
 				}
 			}
 			else {
@@ -356,11 +362,14 @@ module.exports.work = function(creep)
 		}
 		// initialization path
 		else if (creep.step == 'init') {
+			if (this.DEBUG) console.log('- init ' + task.init + ' step ' + creep.memory.step_pos);
 			if (!creep.move(task.init[creep.memory.step_pos])) {
+				if (this.DEBUG) console.log('- next step');
 				creep.memory.step_pos ++;
 				if (creep.memory.step_pos >= task.init.length) {
 					creep.memory.step = 'work';
 					creep.memory.step_pos = task.path.length;
+					if (this.DEBUG) console.log('- work ' + task.path);
 				}
 			}
 			else {
@@ -370,19 +379,28 @@ module.exports.work = function(creep)
 		// work path
 		else if (creep.step == 'work') {
 			if (task.path) {
+				if (this.DEBUG) console.log('- work ' + task.path + ' step ' + creep.memory.step_pos);
 				// - start point : source job (fill)
 				if (creep.memory.step_pos >= task.path.length) {
+					if (this.DEBUG) console.log('- fill');
 					if (!this.fill(creep)) {
+						if (this.DEBUG) console.log('- loop');
 						creep.memory.step_pos = 4;
 					}
 				}
 				// - arrival point : target job
 				if (task.path[creep.memory.step_pos] == path.WAYPOINT) {
+					if (this.DEBUG) console.log('- waypoint');
 					let target = Game.getObjectById(creep.memory.target);
+					if (!target) {
+						target = this.findTarget(creep);
+					}
 					if (this.targetJobDone(creep, target)) {
+						if (this.DEBUG) console.log('- target job done : next step');
 						creep.memory.step_pos ++;
 					}
 					else {
+						if (this.DEBUG) console.log('- execute target job');
 						let error = this.targetJob(creep, target);
 						if (error) {
 							creep.say(error.toString());
@@ -401,8 +419,16 @@ module.exports.work = function(creep)
 				}
 			}
 			else {
+				if (this.DEBUG) console.log('- work');
 				if (!this.fill(creep)) {
-					creep.say('no-fill');
+					let error;
+					let target = Game.getObjectById(creep.memory.target);
+					if (!target || this.targetJobDone(creep, target)) {
+						target = this.findTarget(creep);
+					}
+					if (error = this.targetJob(creep, target)) {
+						creep.say(error.toString());
+					}
 				}
 			}
 		}
@@ -412,9 +438,9 @@ module.exports.work = function(creep)
 		if (!this.fill(creep, this.find_next_target)) {
 			let target = Game.getObjectById(creep.memory.target);
 			if (!target || this.targetJobDone(creep, target)) {
-				this.findTarget(creep);
+				target = this.findTarget(creep);
 			}
-			else if (this.targetJob(creep, target) == ERR_NOT_IN_RANGE) {
+			if (this.targetJob(creep, target) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(target);
 			}
 		}
