e94e8fd14730b899b92b64fcb522eaa178d9fd5b
Baptiste Pillot <baptiste@pillot.fr>
2016-07-22 01:13:12 +0200

    Build rooms roads

--- roads.js
+++ roads.js
@@ -3,26 +3,28 @@
  */
 
 var Path = require('./path');
-var Room = require('./room');
 
 module.exports.build = function()
 {
 	// build roads for all paths having a waypoint (before the waypoint only)
-	for (let task in Memory.tasks) if (Memory.tasks.hasOwnProperty(task)) {
-		task = Memory.tasks[task];
-		if (task.role == 'carrier') {
-			this.buildRoad(task.path.substr(0, task.path.indexOf(Path.WAYPOINT)));
+	for (let room_name in Memory.rooms) if (Memory.rooms.hasOwnProperty(room_name)) {
+		let room_memory = Memory.rooms[room_name];
+		for (let room_role in room_memory) if (room_memory.hasOwnProperty(room_role)) {
+			let memory = room_memory[room_role];
+			if (memory.path) {
+				this.buildRoad(Game.rooms[room_name], memory.path.substr(0, memory.path.indexOf(Path.WAYPOINT)));
+			}
 		}
 	}
 };
 
 /**
+ * @param room Room
  * @param path string 'xxyy123'
  */
-module.exports.buildRoad = function(path)
+module.exports.buildRoad = function(room, path)
 {
 	for (let pos of Path.unserialize(path)) {
-		console.log('build road at ' + pos.x + ', ' + pos.y);
-		Room.room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
+		room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
 	}
 };
