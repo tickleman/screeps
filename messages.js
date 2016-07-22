ff6f40724fce8e873598376b780c20f75e3e507e
Baptiste Pillot <baptiste@pillot.fr>
2016-07-23 01:11:21 +0200

    Fix messages

--- messages.js
+++ messages.js
@@ -3,29 +3,22 @@
  * @type string[]
  */
 module.exports.ERRORS = {
-	ERR_BUSY:                  'BUSY',
-	ERR_FULL:                  'FULL',
-	ERR_GCL_NOT_ENOUGH:        'GCL-LOW',
-	ERR_INVALID_ARGS:          'INVALID-ARGS',
-	ERR_INVALID_TARGET:        'BAD-TARGET',
-	ERR_NAME_EXISTS:           'NAME-EXISTS',
-	ERR_NO_BODYPART:           'NO-BODY',
-	ERR_NO_PATH:               'NO-PATH',
-	ERR_NOT_ENOUGH_ENERGY:     'NO-ENERGY',
-	ERR_NOT_ENOUGH_EXTENSIONS: 'MISS-EXTENSIONS',
-	ERR_NOT_ENOUGH_RESOURCES:  'NO-RESOURCE',
-	ERR_NOT_IN_RANGE:          'NO-RANGE',
-	ERR_NOT_FOUND:             'NOT-FOUND',
-	ERR_NOT_OWNER:             'NOT-OWNER',
-	ERR_RCL_NOT_ENOUGH:        'RCL-LOW',
-	ERR_TIRED:                 'TIRED',
-	OK:                        'OK'
+	'0':   'OK',
+	'-1':  'NOT-OWNER',
+	'-2':  'NO-PATH',
+	'-3':  'NAME-EXISTS',
+	'-4':  'BUSY',
+	'-5':  'NOT-FOUND',
+	'-6':  'NO-ENERGY',
+	'-7':  'BAD-TARGET',
+	'-8':  'FULL',
+	'-9':  'NO-RANGE',
+	'-10': 'INVALID-ARGS',
+	'-11': 'TIRED',
+	'-12': 'NO-BODY',
+	'-14': 'RCL-LOW',
+	'-15': 'GCL-LOW'
 };
-
-/**
- * @type string
- */
-module.exports.UNKNOWN = '?';
 
 /**
  * Gets a short message matchnig the error code
@@ -34,5 +27,5 @@ module.exports.UNKNOWN = '?';
  */
 module.exports.error = function(error)
 {
-	return this.ERRORS[error] ? this.ERRORS[error] : this.UNKNOWN;
+	return this.ERRORS[error.toString()] ? this.ERRORS[error] : error;
 };
