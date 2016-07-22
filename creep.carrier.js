75077c398226a865112412208414d18e09facb18
Baptiste Pillot <baptiste@pillot.fr>
2016-07-23 00:26:04 +0200

    Carrier should really load towers

--- creep.carrier.js
+++ creep.carrier.js
@@ -59,32 +59,26 @@ module.exports.targets = function(context)
 	// next target : towers with less than 90% energy
 	var target = context.pos.findClosestByRange(
 		FIND_MY_STRUCTURES,
-		{ filter: structure => (structure.structureType == STRUCTURE_TOWER) && ((structure.hits / structure.hitsMax) < .9) }
+		{ filter: structure => (structure.structureType == STRUCTURE_TOWER) && (objects.energyRatio(structure) < .9) }
 	);
 	if (target) return [target];
 	for (let ratio in [.5, .7, .9]) {
 		// next target : builder creeps
 		target = context.pos.findClosestByRange(
 			FIND_MY_CREEPS,
-			{ filter: creep => (creep.memory.role == 'builder') && (creep.carry.energy / creep.carryCapacity < ratio) }
+			{ filter: creep => (creep.memory.role == 'builder') && (objects.energyRatio(creep) < ratio) }
 		);
 		if (target) break;
 		// next target : upgrader creeps
 		target = context.pos.findClosestByRange(
 			FIND_MY_CREEPS,
-			{ filter: creep => (creep.memory.role == 'upgrader') && (creep.carry.energy / creep.carryCapacity < ratio) }
+			{ filter: creep => (creep.memory.role == 'upgrader') && (objects.energyRatio(creep) < ratio) }
 		);
 		if (target) break;
 	}
-	// the creep that have the less energy first
-	/*
-	targets.sort(function(creep1, creep2) {
-		var fill1 = creep1.carry.energy / creep1.carryCapacity;
-		var fill2 = creep2.carry.energy / creep2.carryCapacity;
-		if (fill1 < fill2) return -1;
-		if (fill1 > fill2) return 1;
-		return 0;
-	});
-	*/
+	target = context.pos.findClosestByRange(
+		FIND_MY_STRUCTURES,
+		{ filter: structure => (structure.structureType == STRUCTURE_TOWER) && !objects.energyFull(structure) }
+	);
 	return target ? [target] : [];
 };
