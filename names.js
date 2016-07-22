67f4ff7dd46df8d062372aa5244e2dc9e9feabb6
Baptiste Pillot <baptiste@pillot.fr>
2016-07-22 18:38:15 +0200

    I name my creeps with the names of Game of Thrones

--- names.js
+++ names.js
@@ -4,11 +4,30 @@ module.exports.bappli = [
 	'Sebastien', 'Thomas', 'Zhuo'
 ];
 
-module.exports.fightclub = [
+module.exports.fight_club = [
 	'Brad', 'David', 'Edward', 'Eion', 'Jared', 'Helena', 'Marla', 'Meat', 'Richard', 'Ricky', 'Robert', 'Tyler', 'Zach'
 ];
 
-module.exports.use_names = ['bappli', 'fightclub'];
+// source : https://fr.wikipedia.org/wiki/Personnages_de_Game_of_Thrones
+module.exports.game_of_thrones = {
+	'Robin', 'Lysa', 'Anya', 'Yohn', 'Donnel', 'Mord', 'Shagga', 'Timett', 'Hugh', 'Vardis', 'Tommen', 'Robert',
+	'Joffrey', 'Myrcella', 'Stannis', 'Selyse', 'Shirren', 'Davos', 'Melisandre', 'Matthos', 'Cressen', 'Renly', 'Ramsay',
+	'Roose', 'Walda', 'Myranda', 'Locke', 'Walder', 'Lothar', 'Theon', 'Balon', 'Yara', 'Euron', 'Dagmer', 'Tyrion',
+	'Jaime', 'Cersei', 'Kevan', 'Lancel', 'Tywin', 'Alton', 'Martyn', 'Willem', 'Gregor', 'Amory', 'Polliver', 'Tickler',
+	'Doran', 'Ellaria', 'Trystane', 'Tyene', 'Nymeria', 'Obara', 'Areo', 'Oberyn', 'Jon', 'Sansa', 'Arya', 'Bran',
+	'Rickon', 'Eddard', 'Catelyn', 'Robb', 'Talisa', 'Osha', 'Hodor', 'Meera', 'Jojen', 'Luwin', 'Rodrik', 'Jory', 'Nan',
+	'Septa', 'Rockard', 'Torhen', 'Daenerys', 'Viserys', 'Jorah', 'Daario', 'Missandei', 'GreyWorm', 'Barristan',
+	'Kovarro', 'Rakharo', 'Doreah', 'Irri', 'Edmure', 'Brynden', 'Roslin', 'Hoster', 'Margaery', 'Olenna', 'Loras',
+	'Mace', 'Petyr', 'Varys', 'Pycelle', 'Qyburn', 'Ilyn', 'Hallyne', 'Meryn', 'Dontos', 'Brienne', 'Podrick', 'Bronn',
+	'Sandor', 'Gendry', 'Marillion', 'Tobho', 'Beric', 'Thoros', 'Anguy', 'Shae', 'Ros', 'Lommy', 'Rorge', 'Biter',
+	'Syrio', 'Mycah', 'Jaqen', 'Tychos', 'Salladhor', 'Yezzan', 'Illyrio', 'Quaithe', 'Drogo', 'Dizdahr', 'Qotho',
+	'Mirri', 'Xaro', 'Pyat', 'Kraznys', 'Prendahl', 'Samwell', 'Alliser', 'Eddison', 'Othell', 'Olly', 'Bower', 'Benjen',
+	'Jeor', 'Aemon', 'Janos', 'Yoren', 'Qhorin', 'Green', 'Rast', 'Pypar', 'Karl', 'Will', 'Waymar', 'Tormund', 'Vere',
+	'Leaf', 'Mance', 'Ygrid', 'Cliquefrac', 'Orell', 'Craster', 'Styr', 'Wun', 'Drogon', 'Rhaegal', 'Viserion', 'Summer',
+	'Ghost', 'Shaggydog', 'Greywind', 'Lady'
+};
+
+module.exports.use_names = ['game_of_thrones', 'fight_club', 'bappli'];
 
 module.exports.chooseName = function()
 {
