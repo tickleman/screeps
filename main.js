3c4a19f51485ad7d01635413256aa8135f67ba5c
Baptiste Pillot <baptiste@pillot.fr>
2016-07-28 15:00:09 +0200

    Spawn functions from main to spawner

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ var objects    = require('./objects');
 var orders     = require('./orders');
 var repairer   = require('./creep.repairer');
 var rooms      = require('./rooms');
+var spawner    = require('./spawner');
 var tower      = require('./structure.tower');
 var upgrader   = require('./creep.upgrader');
 
 /**
  * @type object|base_creep[]
  */
-var creep_of = {
+module.exports.creep_of = {
 	base_creep: base_creep,
 	builder:    builder,
 	carrier:    carrier,
@@ -26,7 +27,7 @@ var creep_of = {
 /**
  * @type object|tower[]
  */
-var structure_of = {
+module.exports.structure_of = {
 	'tower': tower
 };
 
@@ -39,6 +40,7 @@ module.exports.loop = function ()
 	objects.cache = {};
 	rooms.rooms   = {};
 
+	// count, memorize
 	this.count = creeps.count();
 	if (!rooms.memorized()) rooms.memorize();
 
@@ -48,8 +50,8 @@ module.exports.loop = function ()
 	// structures work
 	for (let structure in Game.structures) if (Game.structures.hasOwnProperty(structure)) {
 		structure = Game.structures[structure];
-		if (structure.my && structure_of[structure.structureType]) {
-			structure_of[structure.structureType].run(structure);
+		if (structure.my && this.structure_of[structure.structureType]) {
+			this.structure_of[structure.structureType].run(structure);
 		}
 	}
 
@@ -58,101 +60,14 @@ module.exports.loop = function ()
 
 	// spawn creeps
 	rooms.forEach(function(room) {
-		let spawn = rooms.get(room, 'spawn');
-		if (spawn && !spawn.spawning) {
-			// spawn the first needed creep
-			if (!main.count[room.name]) {
-				main.spawnSimpleCreep(room, { accept_little: true, count: 1, role: 'carrier' });
-			}
-			// spawn other creeps
-			else if (
-				   main.spawnRoleCreep(room, 'spawn_harvester', { accept_little: true })
-				|| main.spawnSpawnCarrier(room)
-				|| main.spawnRoleCreep(room, 'controller_upgrader', { accept_little: true })
-				|| main.spawnRoleCreep(room, 'controller_harvester', { accept_little: true })
-				|| main.spawnRoleCreep(room, 'controller_carrier', { accept_little: true })
-				|| main.spawnSimpleCreep(room, { accept_little: true, count: 3, role: 'carrier' })
-				|| main.spawnSimpleCreep(room, { count: 1, role: 'builder' })
-				|| main.spawnSimpleCreep(room, { count: 1, role: 'repairer' })
-			) {
-				return true;
-			}
-		}
+		spawner.room(main, room);
 	});
 
 	// creeps work
 	creeps.forEach(function(creep) {
-		if (!creep.spawning && creep_of[creep.memory.role]) {
-			creep_of[creep.memory.role].work(creep);
+		if (!creep.spawning && this.creep_of[creep.memory.role]) {
+			this.creep_of[creep.memory.role].work(creep);
 		}
 	});
 
-};
-
-/**
- * @param room
- * @returns boolean
- */
-module.exports.spawnSpawnCarrier = function(room)
-{
-	return (this.count[room.name].carrier < 2)
-		? this.spawnRoleCreep(room, 'spawn_carrier', { accept_little: true })
-		: false;
-};
-
-/**
- * @param room      Room
- * @param room_role string @example 'spawn_harvester'
- * @param [opts]    object spawn options
- * @returns boolean
- */
-module.exports.spawnRoleCreep = function(room, room_role, opts)
-{
-	if (!opts) opts = {};
-	if (room.controller.level == 1) {
-		opts.accept_little = true;
-	}
-	if (!rooms.has(room, room_role)) {
-		console.log('wish to spawn a ', room_role);
-		let role  = rooms.get(room, room_role, 'role');
-		let spawn = opts.spawn ? opts.spawn : rooms.get(room, 'spawn');
-		if (role && spawn) {
-			if (!opts.role)  opts.role  = role;
-			if (!opts.spawn) opts.spawn = spawn;
-			let creep = creep_of[role].spawn(opts);
-			if (creep) {
-				creep.memory.room          = room.name;
-				creep.memory.room_role     = room_role;
-				if (rooms.get(room, room_role, 'source')) creep.memory.single_source = true;
-				if (rooms.get(room, room_role, 'target')) creep.memory.single_target = true;
-				rooms.setCreep(room, room_role, creep);
-				return true;
-			}
-		}
-	}
-	return false;
-};
-
-/**
- * @param room   Room
- * @param [opts] object spawn options
- * @returns boolean
- */
-module.exports.spawnSimpleCreep = function(room, opts)
-{
-	if (!opts) opts = {};
-	if (this.count[room.name] && this.count[room.name][opts.role] && (this.count[room.name][opts.role] >= opts.count)) {
-		return false;
-	}
-	let spawn = opts.spawn ? opts.spawn : rooms.get(room, 'spawn');
-	console.log(opts.role, 'targets ?');
-	if (creep_of[opts.role].targets(spawn).length) {
-		console.log('> wish to spawn a ', opts.role);
-		if (!opts.spawn) opts.spawn = spawn;
-		let creep = creep_of[opts.role].spawn(opts);
-		if (creep) {
-			return true;
-		}
-	}
-	return false;
 };
---
+++ spawner.js
@@ -0,0 +1,107 @@
+
+var rooms = require('./rooms');
+
+module.exports.room = function(main, room)
+{
+	let spawn = rooms.get(room, 'spawn');
+	if (spawn && !spawn.spawning) {
+		// spawn the first needed creep
+		if (!main.count[room.name]) {
+			this.simpleCreep(main, room, { accept_little: true, count: 1, role: 'carrier' });
+		}
+		// spawn other creeps
+		else if (
+			this.spawnHarvester (main, room)
+			|| this.spawnCarrier(main, room)
+			|| this.roleCreep   (main, room, 'controller_upgrader',  { accept_little: true })
+			|| this.roleCreep   (main, room, 'controller_harvester', { accept_little: true })
+			|| this.roleCreep   (main, room, 'controller_carrier',   { accept_little: true })
+			|| this.simpleCreep (main, room, { accept_little: true, count: 3, role: 'carrier' })
+			|| this.simpleCreep (main, room, { count: 1, role: 'builder' })
+			|| this.simpleCreep (main, room, { count: 1, role: 'repairer' })
+		) {
+			return true;
+		}
+	}
+};
+
+/**
+ * @param main object
+ * @param room Room
+ * @returns boolean
+ */
+module.exports.spawnCarrier = function(main, room)
+{
+	return (main.count[room.name].carrier < 2)
+		? this.roleCreep(main, room, 'spawn_carrier', { accept_little: true })
+		: false;
+};
+
+/**
+ * @param main object
+ * @param room Room
+ * @returns boolean
+ */
+module.exports.spawnHarvester = function(main, room)
+{
+	return this.roleCreep(main, room, 'spawn_harvester', { accept_little: true });
+};
+
+/**
+ * @param main      object
+ * @param room      Room
+ * @param room_role string @example 'spawn_harvester'
+ * @param [opts]    object spawn options
+ * @returns boolean
+ */
+module.exports.roleCreep = function(main, room, room_role, opts)
+{
+	if (!opts) opts = {};
+	if (room.controller.level == 1) {
+		opts.accept_little = true;
+	}
+	if (!rooms.has(room, room_role)) {
+		console.log('wish to spawn a ', room_role);
+		let role  = rooms.get(room, room_role, 'role');
+		let spawn = opts.spawn ? opts.spawn : rooms.get(room, 'spawn');
+		if (role && spawn) {
+			if (!opts.role)  opts.role  = role;
+			if (!opts.spawn) opts.spawn = spawn;
+			let creep = main.creep_of[role].spawn(opts);
+			if (creep) {
+				creep.memory.room          = room.name;
+				creep.memory.room_role     = room_role;
+				if (rooms.get(room, room_role, 'source')) creep.memory.single_source = true;
+				if (rooms.get(room, room_role, 'target')) creep.memory.single_target = true;
+				rooms.setCreep(room, room_role, creep);
+				return true;
+			}
+		}
+	}
+	return false;
+};
+
+/**
+ * @param main   object
+ * @param room   Room
+ * @param [opts] object spawn options
+ * @returns boolean
+ */
+module.exports.simpleCreep = function(main, room, opts)
+{
+	if (!opts) opts = {};
+	if (main.count[room.name] && main.count[room.name][opts.role] && (main.count[room.name][opts.role] >= opts.count)) {
+		return false;
+	}
+	let spawn = opts.spawn ? opts.spawn : rooms.get(room, 'spawn');
+	console.log(opts.role, 'targets ?');
+	if (main.creep_of[opts.role].targets(spawn).length) {
+		console.log('> wish to spawn a ', opts.role);
+		if (!opts.spawn) opts.spawn = spawn;
+		let creep = main.creep_of[opts.role].spawn(opts);
+		if (creep) {
+			return true;
+		}
+	}
+	return false;
+};
